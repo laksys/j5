16dcd860faa7104259e82cd9b0178bd920cb3689
raspberrypi <laksys.pi@gmail.com>
2021-08-01 16:03:56 +0530

    feature java file was added

---
+++ Features.java
@@ -0,0 +1,12 @@
+import static java.lang.System.out;
+
+public class Features {
+	public static void main(String... args){
+		out.prinltn("-------features released in 5------");
+		out.println("auto-boxing, unboxing");
+		out.println("generic");
+		out.println("collection enhancement");
+		out.println("annotation");
+		out.println("----------------------");	
+	}
+}
